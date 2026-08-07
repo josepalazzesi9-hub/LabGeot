c77c1ee60d0e9ffb70c70bb80f5bfba6320a8dc8
MateoAsensiTrujillo <adrielasensi@gmail.com>
2026-08-07 15:26:12 -0300

    Cambios capas y fuga gas.

---
+++ data/puntos_encuentro.js
@@ -0,0 +1,50 @@
+const puntosEncuentroData = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -62.270019,
+          -38.7656967
+        ]
+      },
+      "properties": {
+        "id": 1,
+        "direc": "Punto de encuentro",
+        "ins": "Av. Dasso 2868"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -62.2532661,
+          -38.7706519
+        ]
+      },
+      "properties": {
+        "id": 2,
+        "direc": "Punto de encuentro",
+        "ins": "RN 3 km 683,5"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -62.2654283,
+          -38.783522
+        ]
+      },
+      "properties": {
+        "id": 3,
+        "direc": "Punto de encuentro",
+        "ins": "Avenida San Martin 3619"
+      }
+    }
+  ]
+};
---
+++ data/sirenas.js
@@ -0,0 +1,35 @@
+const sirenasData = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "id": 1,
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -62.2702565,
+          -38.7738449
+        ]
+      },
+      "properties": {
+        "Direc": "Lautaro 2700",
+        "Inst": "Compus Scout Ernesto Pilling"
+      }
+    },
+    {
+      "type": "Feature",
+      "id": 2,
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -62.2741515,
+          -38.7730373
+        ]
+      },
+      "properties": {
+        "Direc": "Avenida Dasso 3475",
+        "Inst": "Centro de Formación Profesional 401"
+      }
+    }
+  ]
+};
